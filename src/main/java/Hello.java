3a0a3407ec709d13c747ba7e454e648c0201954e
Giorgos Klapaftis <t8240063@aueb,gr>
2025-10-26 23:11:44 +0200

    Added a special and absolutely necessary comment

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
      public static void main(String[] args) {
-    System.out.println("Hello from the Team of GovPro!");
+    System.out.println("Hello from the fabulous Team of GovPro!");
+    
     }
 }
 
