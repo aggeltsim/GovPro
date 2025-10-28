cc07ce6070e041946e995de8b3f7f1fccfd06303
Angeliki Tsimidaki <t8240152@aueb.gr>
2025-10-28 22:49:09 +0100

    Changed the HelloTest.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,17 @@
 import java.util.ArrayList;
 
 public class Main {
+  
     public static void main(String args[]) {
-        // Incomes List
+        //Incomes List
         ArrayList <Income> incomes = new ArrayList<>();
-        
+
         var taxes_111 = new Taxes();  //Taxes on goods and services
         var taxes_112 = new Taxes();  //Taxes and duties on imports
         var taxes_113 = new Taxes();  //Property taxes
         var taxes_114 = new Taxes();  //Other taxes on production
         var taxes_115 = new Taxes();  //Income taxes
-        var taxes_116 = new Taxes();  //Capital taxes
+        var taxes_116 = new Taxes();  //Capital Taxes
         var taxes_119 = new Taxes();  //Other current taxes
 
         incomes.add(taxes_111);
@@ -26,10 +27,10 @@ public class Main {
         incomes.add(socialcontr_122);
 
         var transfers_131 = new Transfers();  //Current Internal transfers
-        var transfers_132 = new Transfers();  //Current transfers from organizations and member states of EE
-        var transfers_133 = new Transfers();  //Current transfers from foreign entities
+        var transfers_132 = new Transfers();  //Current transfers from organizations and states of EE
+        var transfers_133 = new Transfers();  //Current transfers from foreign entities 
         var transfers_134 = new Transfers();  //Internal Investments Grants
-        var transfers_135 = new Transfers();  //Investments Grants of EE
+        var transfers_135 = new Transfers();  //Investements Grants of EE
         var transfers_139 = new Transfers();  //Other capital transfers
 
         incomes.add(transfers_131);
@@ -38,12 +39,12 @@ public class Main {
         incomes.add(transfers_134);
         incomes.add(transfers_135);
         incomes.add(transfers_139);
-
+        
         var sales_141 = new Sales();  //Goods Sales
         var sales_142 = new Sales();  //Sales from services
         var sales_143 = new Sales();  //Rents
-        var sales_144 = new Sales();  //Supplies 
-        var sales_145 = new Sales();  //Directors fees
+        var sales_144 = new Sales();  //Supplies
+        var sales_145 = new Sales();  //Director's fees
         var sales_149 = new Sales();  //Other sales
 
         incomes.add(sales_141);
@@ -55,7 +56,7 @@ public class Main {
 
         var other_151 = new Other_Current();  //Incomes from interests
         var other_152 = new Other_Current();  //Distributed corporate income
-        var other_153 = new Other_Current();  //Incomes from Natural resource rents
+        var other_153 = new Other_Current();  //Incomes from natural resource rents
         var other_156 = new Other_Current();  //Incomes from fines, penalties and attributions
         var other_159 = new Other_Current();  //Incomes from expense reimbursements
 
@@ -73,7 +74,7 @@ public class Main {
 
         incomes.add(deptSecurities_432);
 
-        var loans_442 = new Loans();  //Incomes from long-term loans from Recovery Fund resources
+        var loans_442 = new Loans();  //Incomes from long-term loans from Recovery Funds
 
         incomes.add(loans_442);
 
@@ -84,16 +85,16 @@ public class Main {
         incomes.add(securities_452);
 
         var currencyLiabilities = new Currency_Liabilities();
-         
+        
         incomes.add(currencyLiabilities);
 
-        var deptSecurities_531 = new Dept_Securities_L();
-        var deptSecurities_532 = new Dept_Securities_L();
+        var deptSecurities_531 = new Dept_Securities(); 
+        var deptSecurities_532 = new Dept_Securities();
 
         incomes.add(deptSecurities_531);
         incomes.add(deptSecurities_532);
 
-        var loans_541 = new Loans();  //Incomes form short-term internal loans
+        var loans_541 = new Loans();  //Incomes from short-term internal loans
         var loans_542 = new Loans();  //Incomes from long-term external loans
 
         incomes.add(loans_541);
@@ -102,7 +103,7 @@ public class Main {
         var derivatives_571 = new Derivatives();
 
         incomes.add(derivatives_571);
-        
+
         //End of Incomes List
 
         //Expenses List
@@ -120,7 +121,7 @@ public class Main {
         expenses.add(new Valuables());
         expenses.add(new Loans_44());
         expenses.add(new Securities());
-        expenses.add(new Dept_Securties());
+        expenses.add(new Dept_Securities());
         expenses.add(new Loans_54());
 
         //End of Expenses List
@@ -146,7 +147,7 @@ public class Main {
         entities.add(new Ministry_Infrastructure());
         entities.add(new Ministry_Shipping());
         entities.add(new Ministry_Tourism());
-        entities.add(new Ministry_Digital());
+        entiites.add(new Ministry_Digital());
         entities.add(new Ministry_Immigration());
         entities.add(new Ministry_Civil());
         entities.add(new Ministry_Climate());
@@ -165,9 +166,6 @@ public class Main {
 
 
 
-
     }
-
-    
     
 }
--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,6 +1,7 @@
 public class HelloTest {
    public static void main (String[] args) {
        System.out.println("Hello from the testing part of this project!");
+       System.out.println("hello");
    }
 }
 
