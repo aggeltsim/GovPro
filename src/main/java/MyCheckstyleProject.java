00574a59639025eae781036d9ffc1af2f8900927
Angeliki Tsimidaki <t8240152@aueb.gr>
2025-10-30 20:02:44 +0100

    Made a checkstyle test project to see how checkstyle works.

---
+++ src/main/java/MyCheckstyleProject.java
@@ -0,0 +1,15 @@
+/**
+ * Example class for Checkstyle testing.
+ */ 
+
+public class MyCheckstyleProject {
+     
+  /**
+   * Entry point of the program.
+    
+   * @param args command line arguments
+   */
+  public static void main(String[] args) {
+    System.out.println("This is a test for Checkstyle");
+  }
+}
