83b64e4b2d73f38c52422d95cb3a104ff28548cc
Dimitra Tzina <t8240142@aueb.gr>
2025-10-30 10:46:28 +0100

    Changed some files in our testing sessions

---
+++ src/test/java/AnotherTest.java
@@ -0,0 +1,15 @@
+public class AnotherTest {
+    static String text="This is just another test to practice code completion.";
+    public static void getText(String a) {
+        System.out.println(a);
+
+    }
+
+    public static void main(String[] args) {
+      getText(text);
+    }
+
+}
+
+    
+
