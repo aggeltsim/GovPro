ac732c6d7f9d605f1e3492fbde20c00fc2b71918
Ioanna Chatzinikolaou <t8240166@aueb.gr>
2025-11-09 16:14:10 +0000

    This class named DeptSecurLiabil is about Dept Securities (Liabilities).

---
+++ src/main/java/DeptSecurLiabil.java
@@ -0,0 +1,10 @@
+public abstract class DeptSecurLiabil extends Income {
+    
+    public DeptSecurLiabil(String code, String name, double amount, String form) {
+        super(code, name, amount, form);
+    }
+
+    public abstract String toExplain();
+    public abstract String toString();
+
+}
