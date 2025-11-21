3c0fce4de84a700c7dd1b6f35988549915c397b0
Λάουρα Τσάκωνα <t8240149@aueb.gr>
2025-11-21 19:57:52 +0200

    Προσθήκη μεθόδου toExplain

--- src/main/java/entities/MinEnvirEnergy.java
+++ src/main/java/entities/MinEnvirEnergy.java
@@ -2,29 +2,42 @@ package entities;
 
 import java.math.BigDecimal;
 
-public class MinEnvirEnergy  extends Entity{
+public class MinEnvirEnergy extends Entity {
 
     public MinEnvirEnergy(String code, String name, BigDecimal amount) {
         super(code, name, amount);
     }
 
     //Getters and Setters
-    public String getCode() { return code; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     //getters and setters for amount
-    public BigDecimal getAmount() { return amount; }
-    public void setAmount(BigDecimal amount) { this.amount = amount; }
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 
     @Override
     public String toExplain() {
-        return "Responsible for national defense, military forces, and defense policy.";
+        return getName() + "Oversees environmental protection, energy policy, and natural resources.";
     }
 
     @Override
     public String toString() {
         return "Account: " + getName() + "\n" + "Amount: " + getAmount();
     }
-    
+
 }
