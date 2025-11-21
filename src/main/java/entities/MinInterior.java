29e939b83195a5d047afc44769752b6316c8d5cf
Λάουρα Τσάκωνα <t8240149@aueb.gr>
2025-11-21 19:49:36 +0200

    Προσθήκη μεθόδου toExplain

--- src/main/java/entities/MinInterior.java
+++ src/main/java/entities/MinInterior.java
@@ -2,25 +2,37 @@ package entities;
 
 import java.math.BigDecimal;
 
-public class MinInterior extends Entity{
-    
+public class MinInterior extends Entity {
 
     public MinInterior(String code, String name, BigDecimal amount) {
         super(code, name, amount);
     }
 
     //Getters and Setters
-    public String getCode() { return code; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     //getters and setters for amount
-    public BigDecimal getAmount() { return amount; }
-    public void setAmount(BigDecimal amount) { this.amount = amount; }
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 
     @Override
     public String toExplain() {
-        return "Responsible for national defense, military forces, and defense policy.";
+        return getName() + "Responsible for public administration, local government, elections, and civil status matters.";
     }
 
     @Override
