3f75454f3e2ac1437145d9be49341282f37d9769
Giorgos Klapaftis <t8240063@aueb,gr>
2025-11-23 15:59:10 +0200

    Fix minor error

--- src/main/java/entities/DecAdminAttica.java
+++ src/main/java/entities/DecAdminAttica.java
@@ -4,8 +4,8 @@ import java.math.BigDecimal;
 
 public class DecAdminAttica extends Entity{
 
-    public DecAdminAttica(String code, String name, BigDecimal amount, String year) {
-        super(code, name, amount, year);
+    public DecAdminAttica(String code, String name, BigDecimal amount) {
+        super(code, name, amount);
     }
 
     //Getters and Setters
