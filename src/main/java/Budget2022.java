f05a72da5c3a4046d97c7f445c19d87fc981c9ad
Angeliki Tsimidaki <t8240152@aueb.gr>
2026-01-14 17:27:41 +0000

    Make changes according to Checkstyle

--- src/main/java/Budget2022.java
+++ src/main/java/Budget2022.java
@@ -8,42 +8,64 @@ import java.math.RoundingMode;
  * All calculations use {@link BigDecimal} with 8-decimal precision and 
  * {@link RoundingMode#HALF_UP} for financial accuracy.
  */
+
 public class Budget2022 {
-    public static BigDecimal[] staticsExp() {
-        BigDecimal expenses2022[] = {new BigDecimal("13610168000"), new BigDecimal("220643000"), new BigDecimal("30583382000"), 
-                                    new BigDecimal("1355222000"), new BigDecimal("80060000"), new BigDecimal("4600450000"), 
-                                    new BigDecimal("101387000"), new BigDecimal("13921510000"), new BigDecimal("3455707000"), 
-                                    new BigDecimal("56000"), new BigDecimal("3509500000"), new BigDecimal("1062480000"), 
-                                    new BigDecimal("29153000000"), new BigDecimal("703687130000"), new BigDecimal("1400000000")};
-        final BigDecimal totalExp = new BigDecimal("806740695000");
-        BigDecimal[] statics2022 = new BigDecimal[15];
-        for (int i = 0; i < expenses2022.length; i++) {
-            statics2022[i] = BigDecimal.ZERO;
-        }
-        for (int i = 0; i < expenses2022.length; i++) {
-            statics2022[i] = expenses2022[i].divide(totalExp, 8, RoundingMode.HALF_UP); 
-        }
-        return statics2022;
-        
+
+  /**
+   * Calculates and returns the relative distribution of expense categories
+   * for the year 2022 based on the total public expenditure.
+   *
+   * <p>Each value in the returned array represents the proportion of a specific
+   * expense category relative to the total expenses and is computed using
+   * {@link BigDecimal} division with rounding applied.
+   * </p>
+   *
+   * @return a {@link BigDecimal} array containing the normalized expense values
+   */
+  public static BigDecimal[] staticsExp() {
+    BigDecimal[] expenses2022 = {new BigDecimal("13610168000"), new BigDecimal("220643000"), 
+        new BigDecimal("30583382000"), new BigDecimal("1355222000"), 
+        new BigDecimal("80060000"), new BigDecimal("4600450000"), new BigDecimal("101387000"), 
+        new BigDecimal("13921510000"), new BigDecimal("3455707000"), new BigDecimal("56000"), 
+        new BigDecimal("3509500000"), new BigDecimal("1062480000"), new BigDecimal("29153000000"), 
+        new BigDecimal("703687130000"), new BigDecimal("1400000000")};
+    final BigDecimal totalExp = new BigDecimal("806740695000");
+    BigDecimal[] statics2022 = new BigDecimal[15];
+    for (int i = 0; i < expenses2022.length; i++) {
+      statics2022[i] = BigDecimal.ZERO;
     }
+    for (int i = 0; i < expenses2022.length; i++) {
+      statics2022[i] = expenses2022[i].divide(totalExp, 8, RoundingMode.HALF_UP); 
+    }
+    return statics2022;
+        
+  }
 
-        public static BigDecimal[] staticsInc() { 
-             BigDecimal incomes2022[] = {new BigDecimal("45654498000"), new BigDecimal("55000000"), new BigDecimal("7376000000"), 
-                                    new BigDecimal("2092000000"), new BigDecimal("1911000000"), new BigDecimal("13000000"), 
-                                    new BigDecimal("31000000"), new BigDecimal("687000000"), new BigDecimal("6000000"), 
-                                    new BigDecimal("36954000000"), new BigDecimal("701846000000"), new BigDecimal("0")};
-            final BigDecimal totalInc = new BigDecimal("796625498000");
-            BigDecimal[] statics2022 = new BigDecimal[12];
-            for (int i = 0; i < incomes2022.length; i++) {
-               statics2022[i] = BigDecimal.ZERO;
-            }
-            for (int i = 0; i < incomes2022.length; i++) {
-                statics2022[i] = incomes2022[i].divide(totalInc, 8, RoundingMode.HALF_UP); 
-            }
-            return statics2022;
-        }
+  /**
+   * Calculates and returns the relative distribution of income categories
+   * for the year 2022 based on the total public revenue.
+   *
+   * <p>Each element in the returned array represents the proportion of a specific
+   * income category relative to the total income and is calculated using
+   * {@link BigDecimal} division with rounding applied.
+   * </p>
+   *
+   * @return a {@link BigDecimal} array containing the normalized income values
+   */
+  public static BigDecimal[] staticsInc() { 
+    BigDecimal[] incomes2022 = {new BigDecimal("45654498000"), new BigDecimal("55000000"), 
+        new BigDecimal("7376000000"), new BigDecimal("2092000000"), 
+        new BigDecimal("1911000000"), new BigDecimal("13000000"), new BigDecimal("31000000"),
+        new BigDecimal("687000000"), new BigDecimal("6000000"), new BigDecimal("36954000000"), 
+        new BigDecimal("701846000000"), new BigDecimal("0")};
+    final BigDecimal totalInc = new BigDecimal("796625498000");
+    BigDecimal[] statics2022 = new BigDecimal[12];
+    for (int i = 0; i < incomes2022.length; i++) {
+      statics2022[i] = BigDecimal.ZERO;
+    }
+    for (int i = 0; i < incomes2022.length; i++) {
+      statics2022[i] = incomes2022[i].divide(totalInc, 8, RoundingMode.HALF_UP); 
+    }
+    return statics2022;
+  }
 }
-        
-        
-
-
